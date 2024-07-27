3e5b7fe2917160eec92b66388641b96ca1010727
Carl Sinclair <69092633+CarlSinclair@users.noreply.github.com>
2024-07-27 12:31:42 -0300

    Update glossary.php

--- glossary.php
+++ glossary.php
@@ -12,7 +12,7 @@ class GlossaryPlugin extends Plugin
     public static function getSubscribedEvents()
     {
         return [
-            'onPageContentRaw' => ['insertAbbreviations', 0],
+            'onPageInitialized' => ['onPageInitialized', 0],
             'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
             'onGetPageTemplates' => ['onGetPageTemplates', 0],
         ];
@@ -42,53 +42,30 @@ class GlossaryPlugin extends Plugin
      */
     public function insertAbbreviations(Event $event)
     {
+        return;
+    }
+
+    /**
+     * Expose glossary terms to JavaScript
+     */
+    public function onPageInitialized()
+    {
+        // Ensure this method runs only on the front-end
+        if ($this->isAdmin()) {
+            return;
+        }
+
         // Get config
         $pluginConfig = $this->config->get('plugins.glossary', array());
 
-        // Check if abbreviations are enabled
-        if (! array_key_exists("abbreviations", $pluginConfig) ||
-            ! $pluginConfig["abbreviations"]) {
-            return;
-        }
+        // Check if abbreviations are enabled and get terms
+        if (!empty($pluginConfig['abbreviations']) && !empty($pluginConfig['definitions'])) {
+            $terms = $pluginConfig['definitions'];
 
-        // Get the terms if they exist, exit otherwise
-        if (array_key_exists("definitions", $pluginConfig)) {
-            $terms = $pluginConfig["definitions"];
-        } else {
-            return;
-        }
-
-        $page = $event['page'];
-        $config = $this->mergeConfig($page);
-
-        $enabled = $config->get('enabled');
-
-        // Check that page processes markdown
-        $enabled = $enabled && $page->process()['markdown'];
-
-        // Check that markdown extra is enabled
-        if (isset($page->header()->markdown) &&
-            array_key_exists('extra', $page->header()->markdown)) {
-            $enabled = $enabled && $page->header()->markdown['extra'];
-        } else {
-            $enabled = $enabled && $this->config->get('system.pages.markdown.extra');
-        }
-
-        // Inject the abbreviations into the page
-        if ($enabled && (count($terms) > 0)) {
-            // Get initial content
-            $raw = $page->getRawContent();
-            $raw .= "\n\n";
-
-            // Append acronyms to page (if abbrev is defined)
-            foreach ($terms as $term) {
-                if (array_key_exists("abbrev", $term)) {
-                    $raw .= "*[${term['abbrev']}]: ${term['term']}\n";
-                }
-            }
-
-            // Put content back in
-            $page->setRawContent($raw);
+            // Add script with glossary terms to the head section
+            $this->grav['assets']->addInlineJs(
+                'window.glossaryTerms = ' . json_encode($terms) . ';'
+            );
         }
     }
 }
